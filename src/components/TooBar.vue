7b48d450426f5de0687b648986e852b842326221
Hanecorin <sdh1541@naver.com>
2023-08-22 17:58:28 +0900

    first commit

---
+++ src/components/TooBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to="/news">News</router-link><br>
+    <router-link to="/ask">Ask</router-link><br>
+    <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
